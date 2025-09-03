5650d0f604affb94c6cfff0a78d9fafaabbd5f49
GlorenBank <gloribethrenram87@gmail.com>
2025-09-03 09:34:24 -0500

    Final commit

--- src/main/java/Executable.java
+++ src/main/java/Executable.java
@@ -2,7 +2,9 @@ import java.util.Random;
 
 public class Executable {
 
-    static int[] array;
+    static int[] array1;
+    static int[] array2;
+    static int[] array3;
     static Random random;
 
     public static void main(String[] args){
@@ -10,20 +12,26 @@ public class Executable {
     }
 
     public static void initializer(){
-        array = new int[10];
-        createArray(array);
+        array1 = new int[10];
+        array2 = new int[10];
+        array3 = new int[10];
+
+        createArray(array1);
+        createArray(array2);
+        createArray(array3);
         methods();
     }
 
     public static void methods(){
-        bubble();
-        selection();
-        insertion();
+        bubble(array1);
+        selection(array2);
+        insertion(array3);
     }
 
 
     public static int[] bubble(int[] array){
         int n = array.length;
+        System.out.println("Bubble:");
 
         for(int i = 0; i < n-1; i++){
             for(int j = 0; j < n-i-1; j++){
@@ -33,12 +41,15 @@ public class Executable {
                     array[j+1] = temp;
                 }
             }
+            System.out.println(toString(array));
         }
         return array;
     }
 
     public static int[] selection(int[] array){
         int n = array.length;
+        System.out.println("Selection:");
+
         for (int i = 0; i < n; i++) {
             int min = i;
             for (int j = i + 1; j < n; j++) {
@@ -49,6 +60,8 @@ public class Executable {
             int temp =  array[i];
             array[i] = array[min];
             array[min] = temp;
+
+            System.out.println(toString(array));
         }
 
         return array;
@@ -56,6 +69,7 @@ public class Executable {
 
     public static int[] insertion(int[] array){
         int n = array.length;
+        System.out.println("Insertion:");
 
         for(int i = 1; i < n; i++){
             int  key = array[i];
@@ -65,6 +79,7 @@ public class Executable {
                 j = j-1;
             }
             array[j+1] = key;
+            System.out.println(toString(array));
         }
 
         return array;
@@ -76,5 +91,13 @@ public class Executable {
         for(int i = 0; i < array.length; i++){
             array[i] = random.nextInt(10);
         }
+    }
+
+    public static String toString(int[] array){
+        String str = "";
+        for(int i = 0; i < array.length; i++){
+            str += array[i] + " ";
+        }
+        return str;
     }
 }
